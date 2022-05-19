562410ec0544abd5ef8e4d755e6d3681d42ff050
Nikita <nikitazacharenko@mail.ru>
2022-05-19 16:47:53 +0300

    started lab04 mail client

---
+++ lab04mail/src/com/zakharenko/lab04mail/controller/Controller.java
@@ -0,0 +1,18 @@
+package com.zakharenko.lab04mail.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
+public class Controller {
+    @FXML
+    public TableView mailTable;
+    @FXML
+    public Button sendButton;
+    @FXML
+    public Button reloadButton;
+
+    public void initialize(){
+
+    }
+}
---
+++ lab04mail/src/com/zakharenko/lab04mail/runner/Main.java
@@ -0,0 +1,25 @@
+package com.zakharenko.lab04mail.runner;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../view/sample.fxml")));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 903, 637));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
